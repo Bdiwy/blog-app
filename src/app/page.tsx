11d10757ccd19124a57e2b42bffdc69c34a033e3
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2025-11-26 01:59:14 +0200

    makes the id number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Blog } from "../types/blog";
 
 const fakeData: Blog[] = [
   {
-    id: "1",
+    id: 1,
     title: "Welcome to My Blog!",
     description: "This is our first blog post using Next.js + TypeScript.",
     image: "https://picsum.photos/600/400?1",
